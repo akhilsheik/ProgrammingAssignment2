32b26c147a2af397a10353bff511434bbc8cee06
akhilsheik <sheik.akhil@cytel.com>
2015-08-21 16:46:19 +0530

    Submitting function for Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,16 @@
-## The makeCacheMatrix holds the invertible matrix as input for the next function
-## cachesolve, which computes the input and returns the inverse of the input matrix,
-## provided the similar input/output is not in the cache.
+## The makeCacheMatrix holds the invertible matrix as an input for the next function.
+## cacheSolve computes the input and returns the inverse of the input matrix,
+## If the value/output is already present in cache, it will then return a message
+## "Getting cached data" along with the result.
 
 ## makeCacheMatrix holds the invertible matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  
+
+  # initialize the inverse matrix to NULL during the first call to makeCacheMatrix  
   inv<-NULL
+  
+  # funciton to set a new value for the underlying martix
   set<-function(y) {
     
     x<<-y
@@ -14,20 +18,35 @@ makeCacheMatrix <- function(x = matrix()) {
     
   }
   
+  # get function for underlying matrix
   get<-function() x
+  
+  # set the Inverse of the matrix x.  Called by cacheSolve
   setinv<-function(inv) inv <<-inv
+  
+  # returns the inverse of matrix x
+  
   getinv<-function() inv
+  
+  # return value of the makeCacheMatrix function is a list
+  
   list(set=set,get=get, setinv=setinv,getinv=getinv)
   
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function will take the cached output from makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  # get the inverse of the matrix defined inside x.
   
   inv <- x$getinv()
+  
+  # If already computed inverse of martix stored via setinverse(),
+  # and have not invalidated the cache by calling set(), 
+  # return the cached version of inverse of martix x 
+
+  
   if(!is.null(inv)) {
     
     message("getting cached data")
@@ -35,9 +54,16 @@ cacheSolve <- function(x, ...) {
     
   }
   
+  # either we havent computed the cached version yet, or we've called
+  # set() previously and invalidated the cache.
+  
+  # call get() to get the underlying matrix
+  
   data <- x$get()
   inv <- solve(data, ...)
   x$setinv(inv) 
+  
+  #return the cached inverse of matrix x
   inv
   
 }
